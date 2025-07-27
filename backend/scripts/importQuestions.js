06f3ddf0a0750fe7d5a230d21f6d65a8619cfeeb
Chahat Chauhan <147340918+Chahat2521@users.noreply.github.com>
2025-07-27 20:30:53 +0530

    importQuestions.js

--- backend/scripts/importQuestions.js
+++ backend/scripts/importQuestions.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Question = require('../models/Question');
 
-mongoose.connect('mongodb://localhost:27017/quiz', {
+mongoose.connect('mongodb+srv://chahatchauhan1616:Chahat123@cluster21.ls9c4hd.mongodb.net/quizDB?retryWrites=true&w=majority&appName=Cluster21'), {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
